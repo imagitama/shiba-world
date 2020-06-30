1a239fcacdf1b02459f032e733b4d88461b43139
Jared Williams <accounts@jaredwilliams.com.au>
2020-06-30 22:56:27 +0800

    Fix build

--- reactapp/src/components/asset-overview/components/pikapetey-discord-message/index.jsx
+++ reactapp/src/components/asset-overview/components/pikapetey-discord-message/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import WarningIcon from '@material-ui/icons/Warning'
