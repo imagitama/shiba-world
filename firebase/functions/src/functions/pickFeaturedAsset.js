5b4decc04be0c7eb1a86efc8d27652f081f0a387
Jared Williams <accounts@jaredwilliams.com.au>
2021-01-12 12:12:01 +0800

    Add rules and make pubsub

--- firebase/functions/src/functions/pickFeaturedAsset.js
+++ firebase/functions/src/functions/pickFeaturedAsset.js
@@ -1,16 +1,6 @@
 const functions = require('firebase-functions')
 const { pickFeaturedAsset } = require('../featuredAssets')
 
-// module.exports = functions.pubsub
-//   .schedule('0 0 * * *') // daily at midnight
-//   .onRun(async () => pickFeaturedAsset())
-
-module.exports = functions.https.onRequest(async (req, res) => {
-  try {
-    await pickFeaturedAsset()
-    res.status(200).send('Featured asset picked')
-  } catch (err) {
-    console.error(err)
-    res.status(500).send(`Error: ${err.message}`)
-  }
-})
+module.exports = functions.pubsub
+  .schedule('0 0 * * *') // daily at midnight
+  .onRun(async () => pickFeaturedAsset())
