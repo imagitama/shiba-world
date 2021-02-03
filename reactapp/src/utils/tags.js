d40e35b12d751b72b34de04e2f03dd7b1d16c600
Jared Williams <accounts@jaredwilliams.com.au>
2021-02-03 11:48:35 +0800

    Fix tags not having numbers

--- reactapp/src/utils/tags.js
+++ reactapp/src/utils/tags.js
@@ -1,7 +1,7 @@
 export const cleanupTags = tags =>
   tags
     ? tags
-        .filter(tag => /^[a-z_]+$/g.test(tag))
+        .filter(tag => /^[a-z0-9_]+$/g.test(tag))
         .map(tag =>
           tag
             .trim()
