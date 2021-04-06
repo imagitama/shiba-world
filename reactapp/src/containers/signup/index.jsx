a181350e3a3d142e3e66e48f92bfb56345b3564f
Jared Williams <accounts@jaredwilliams.com.au>
2021-04-07 03:49:28 +0800

    Warning about error in sign up

--- reactapp/src/containers/signup/index.jsx
+++ reactapp/src/containers/signup/index.jsx
@@ -9,6 +9,7 @@ import LoginForm from '../../components/login-form'
 import Heading from '../../components/heading'
 import BodyText from '../../components/body-text'
 import ErrorMessage from '../../components/error-message'
+import Message, { types } from '../../components/message'
 
 export default ({ history: { push } }) => {
   const [, , user] = useUserRecord()
@@ -27,12 +28,15 @@ export default ({ history: { push } }) => {
     <>
       <Heading variant="h1">Sign Up</Heading>
       <BodyText>Enter your details below to create a new account.</BodyText>
-      <BodyText>
-        <strong>
-          Please note that we do not use your first and last name anywhere on
-          the site but it is stored. You will set a username after you sign up.
-        </strong>
-      </BodyText>
+      <br />
+      <br />
+      <Message type={types.WARNING}>
+        We are aware of issues where creating your profile after your sign up
+        doesn't work. Please report this to #bugs in our Discord server and try
+        an alternative way to sign up.
+      </Message>
+      <br />
+      <br />
       <LoginForm
         onSuccess={() => {
           trackAction('Signup', 'Click sign-up button')
