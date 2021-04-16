afbe8fd6e3ec1858be016eb7fe3cae86ea713330
Jared Williams <accounts@jaredwilliams.com.au>
2021-04-16 09:46:32 +0800

    Fix endorsement button

--- reactapp/src/components/endorse-asset-button/index.jsx
+++ reactapp/src/components/endorse-asset-button/index.jsx
@@ -33,6 +33,11 @@ export default ({ assetId, endorsementCount = '???', onClick = null }) => {
             EndorsementFieldNames.createdBy,
             Operators.EQUALS,
             createRef(CollectionNames.Users, userId)
+          ],
+          [
+            EndorsementFieldNames.asset,
+            Operators.EQUALS,
+            createRef(CollectionNames.Assets, assetId)
           ]
         ]
       : false,
