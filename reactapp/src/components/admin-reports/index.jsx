63e42e1d5f5bd7e626d9570a35f2d47cd001a294
imagitama <1967342+imagitama@users.noreply.github.com>
2021-05-12 19:07:36 +0800

    Toggle deleted reports

--- reactapp/src/components/admin-reports/index.jsx
+++ reactapp/src/components/admin-reports/index.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControl from '@material-ui/core/FormControl'
 
 import useDatabaseQuery, {
   CollectionNames,
-  AssetAmendmentFieldNames,
-  Operators,
-  options
+  options,
+  ReportFieldNames,
+  OrderDirections,
+  Operators
 } from '../../hooks/useDatabaseQuery'
 
 import LoadingIndicator from '../loading-indicator'
 import ErrorMessage from '../error-message'
 import ReportResults from '../report-results'
 
-export default () => {
+const Reports = ({ areDeletedReportsVisible }) => {
   const [isLoading, isErrored, results] = useDatabaseQuery(
     CollectionNames.Reports,
-    [],
+    areDeletedReportsVisible === true
+      ? []
+      : [[ReportFieldNames.isDeleted, Operators.EQUALS, false]],
     {
       [options.subscribe]: true,
       [options.populateRefs]: true,
-      [options.queryName]: 'admin-asset-reports'
+      [options.queryName]: 'admin-asset-reports',
+      [options.orderBy]: [ReportFieldNames.createdAt, OrderDirections.DESC]
     }
   )
 
@@ -35,4 +42,29 @@ export default () => {
   }
 
   return <ReportResults reports={results} showControls showAssetDetails />
+}
+
+export default () => {
+  const [areDeletedReportsVisible, setAreDeletedReportsVisible] = useState(
+    false
+  )
+
+  return (
+    <div>
+      <FormControl>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={areDeletedReportsVisible}
+              onChange={() =>
+                setAreDeletedReportsVisible(currentVal => !currentVal)
+              }
+            />
+          }
+          label="Show deleted reports"
+        />
+      </FormControl>
+      <Reports areDeletedReportsVisible={areDeletedReportsVisible} />
+    </div>
+  )
 }
