0f9d2bcf18025570fdb277d09600a3b8e21358b2
imagitama <accounts@jaredwilliams.com.au>
2021-07-06 19:32:38 +1000

    Redesign streams page

--- reactapp/src/components/streams-list-item/index.jsx
+++ reactapp/src/components/streams-list-item/index.jsx
@@ -1,32 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import LazyLoad from 'react-lazyload'
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
+import { Link } from 'react-router-dom'
 
+import * as routes from '../../routes'
+import Button from '../button'
 import { trackAction } from '../../analytics'
 
 const useStyles = makeStyles({
   root: {
-    width: '50%',
-    flexShrink: 1
+    padding: '1rem'
+  },
+  inner: {
+    border: '1px solid rgba(255, 255, 255, 0.5)',
+    borderRadius: '3px'
+  },
+  twitchUsername: {
+    fontSize: '150%',
+    padding: '0.5rem'
+  },
+  videoArea: {
+    width: '100%',
+    minHeight: '320px',
+    background: '#000',
+    position: 'relative'
+  },
+  watchBtn: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  stopWatchingBtn: {
+    margin: '0.25rem'
   }
 })
 
-export default ({ twitchUsername }) => {
+export default ({ profile: { id: userId, twitchUsername } }) => {
   const classes = useStyles()
+  const [isStreamEnabled, setIsStreamEnabled] = useState(false)
+
   return (
     <div className={classes.root}>
-      <LazyLoad>
-        <ReactTwitchEmbedVideo
-          channel={twitchUsername}
-          targetId={`twitch-embed-${twitchUsername}`}
-          width="100%"
-          layout="video"
-          onPlay={() =>
-            trackAction('Streams', 'Click play stream button', twitchUsername)
-          }
-        />
-      </LazyLoad>
+      <div className={classes.inner}>
+        <div className={classes.twitchUsername}>
+          <a href={`https://twitch.tv/${twitchUsername}`}>{twitchUsername}</a> (
+          <Link to={routes.viewUserWithVar.replace(':userId', userId)}>
+            user
+          </Link>
+          )
+        </div>
+
+        <div className={classes.videoArea}>
+          {isStreamEnabled ? (
+            <div>
+              <ReactTwitchEmbedVideo
+                channel={twitchUsername}
+                targetId={`twitch-embed-${twitchUsername}`}
+                width="100%"
+                layout="video"
+                onPlay={() =>
+                  trackAction(
+                    'Streams',
+                    'Click play stream button',
+                    twitchUsername
+                  )
+                }
+              />
+              <Button
+                size="small"
+                className={classes.stopWatchingBtn}
+                onClick={() => setIsStreamEnabled(false)}>
+                Stop Watching
+              </Button>
+            </div>
+          ) : (
+            <Button
+              className={classes.watchBtn}
+              onClick={() => setIsStreamEnabled(true)}>
+              Watch
+            </Button>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
--- reactapp/src/containers/streams/index.jsx
+++ reactapp/src/containers/streams/index.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 import StreamsList from '../../components/streams-list'
 import Heading from '../../components/heading'
 import BodyText from '../../components/body-text'
+import LoadingIndicator from '../../components/loading-indicator'
+import ErrorMessage from '../../components/error-message'
+import useDatabaseQuery, {
+  CollectionNames,
+  Operators,
+  ProfileFieldNames
+} from '../../hooks/useDatabaseQuery'
+
+function Streams() {
+  const [isLoading, isError, profiles] = useDatabaseQuery(
+    CollectionNames.Profiles,
+    [[ProfileFieldNames.twitchUsername, Operators.GREATER_THAN, '']]
+  )
+
+  if (isLoading || !profiles) {
+    return <LoadingIndicator message="Loading profiles..." />
+  }
+
+  if (isError) {
+    return <ErrorMessage>Failed to load profiles</ErrorMessage>
+  }
+
+  return <StreamsList profiles={profiles} />
+}
 
 export default () => (
   <>
     <Heading variant="h1">Streams</Heading>
     <BodyText>A list of Twitch streams.</BodyText>
-    <StreamsList />
+    <Streams />
   </>
 )
